229e9fedef4265159ca60d0daf7232d91bdee477
Fertille <efeliott@gmail.com>
2022-11-22 18:27:51 +0100

    update du 22/11/2022

--- Sprint/1/Formulaire_participation/php/validForm.php
+++ Sprint/1/Formulaire_participation/php/validForm.php
@@ -1,3 +1,9 @@
+<!--
+    Page crée le 10/11/2022 par Eliott FERTILLE dans le cadre du projet GED'IMAGINATION en 2ème année de BTS
+    Cette page a pour but de traiter les données du formulaire de publication de réalisation du jeu concours de Gédimat
+    Dernière modif : 22/11/2022
+-->
+
 <?php
     //verifie si le boutton submit a bien été actioner
     if(isset($_POST['submit']))
@@ -9,11 +15,17 @@
             // Création de variable pour simplifier le travail
             $file = $_FILES['photo'];
             $fileName = $file['name'];
+            
+            $file_tmp_name = $_FILES['photo']['tmp_name'];
+            $file_dest = 'upload/'.$fileName;
 
             // Bloquage des fichiers qui ne correspondent pas au niveau des extensions
-            if(pathinfo($fileName, PATHINFO_EXTENSION) == 'png' || pathinfo($fileName, PATHINFO_EXTENSION) == 'jpg' || pathinfo($fileName, PATHINFO_EXTENSION) == 'jpeg')
+            if(pathinfo($fileName, PATHINFO_EXTENSION) = 'png' || pathinfo($fileName, PATHINFO_EXTENSION) = 'jpg' || pathinfo($fileName, PATHINFO_EXTENSION) = 'jpeg' || pathinfo($fileName, PATHINFO_EXTENSION) = 'PNG' || pathinfo($fileName, PATHINFO_EXTENSION) = 'JPG' || pathinfo($fileName, PATHINFO_EXTENSION) = 'JPEG')
             {
-                echo "C'est bon pour l'extension";
+                if(move_uploaded_file($file_tmp_name, $file_dest))
+                {
+                    echo "ok up";
+                }
             }
             else
             {
