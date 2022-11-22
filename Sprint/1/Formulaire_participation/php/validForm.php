02299b03a677ea78e1c51d062904897bf2ca7621
Eliott <efeliott@gmail.com>
2022-11-22 14:42:05 +0100

    Travail mardi Girard

--- Sprint/1/Formulaire_participation/php/validForm.php
+++ Sprint/1/Formulaire_participation/php/validForm.php
@@ -2,11 +2,24 @@
     //verifie si le boutton submit a bien été actioner
     if(isset($_POST['submit']))
     {
-        if(isset($_POST['photo']) and isset($_POST['titre']) and isset($_POST['description']) and isset($_POST['debut']) and isset($_POST['fin']))
+        // Verifie si une photo a été déposée
+        if(isset($_FILES['photo']) && !empty($_FILES['photo']) && isset($_POST['titre']) && !empty($_POST['titre']) && isset($_POST['description']) && !empty($_POST['description']) 
+        && isset($_POST['debut']) && !empty($_POST['debut']) && isset($_POST['fin']) && !empty($_POST['fin']))
         {
-            if(!empty($_POST['photo']) and !empty($_POST['titre']) and !empty($_POST['description']) and !empty($_POST['debut']) and !empty($_POST['fin']))
+            // Création de variable pour simplifier le travail
+            $file = $_FILES['photo'];
+            $fileName = $file['name'];
+
+            // Bloquage des fichiers qui ne correspondent pas au niveau des extensions
+            if(pathinfo($fileName, PATHINFO_EXTENSION) == 'png' || pathinfo($fileName, PATHINFO_EXTENSION) == 'jpg' || pathinfo($fileName, PATHINFO_EXTENSION) == 'jpeg')
             {
-                
+                echo "C'est bon pour l'extension";
+            }
+            else
+            {
+                echo "Extension de fichiers son acceptée";
+                // header('Location: http://localhost/Formulaire_participation/');
+                // exit();
             }
         }
     }
