6f54fb165c0fcd2781bc41c24fd1eb07be2188bc
Eliott <efeliott@gmail.com>
2022-11-24 08:47:38 +0100

    update apres train 24/11

--- Sprint/1/Formulaire_participation/php/validForm.php
+++ Sprint/1/Formulaire_participation/php/validForm.php
@@ -8,6 +8,9 @@
     // Appel de mon fichier fonction pour me connecter à la base de donnée
     require '../php/fonction.inc.php';
 
+    // Definition d'une constante pour le poid maximum accepté pour la photo
+    define("MAX_FILE_SIZE", 10485760);
+
     //verifie si le boutton submit a bien été actioner
     if(isset($_POST['submit']))
     {
@@ -28,13 +31,13 @@
             if(pathinfo($file_name, PATHINFO_EXTENSION) == 'png' || pathinfo($file_name, PATHINFO_EXTENSION) == 'jpg' || pathinfo($file_name, PATHINFO_EXTENSION) == 'jpeg' 
             || pathinfo($file_name, PATHINFO_EXTENSION) == 'PNG' || pathinfo($file_name, PATHINFO_EXTENSION) == 'JPG' || pathinfo($file_name, PATHINFO_EXTENSION) == 'JPEG')
             {
-                if($file_size < 300000)
+                if($file_size < MAX_FILE_SIZE)
                 {
                     // Si l'extension de fichiers est bonne et que le poid correspond on envoie la photo vers le server web
                     if(move_uploaded_file($file_tmp_name, $file_dest) && $_FILES['photo']['error'] == 0)
                     {
                         // Insertion de l'url de la photo et de son nom vers la bdd
-                        $req = $cnx->prepare('INSERT INTO realisation(name_photo, url_photo) VALUES(?,?)');
+                        $req = $cnx->prepare('INSERT INTO realisation(photo_name, photo_url) VALUES(?,?)');
                         $req->execute(array($file_name, $file_dest));
                     }
                     else
@@ -54,20 +57,8 @@
                 // exit();
             }
 
-            // Verication de la date de participation comprise dans les temps de participation
-            $request = $cnx->prepare('SELECT participationDebut, participationFin FROM concours');
-            var_dump($request);
-            // Date actuelle
-            $date_ajd = date('d/m/y');
-
-            // if($data['participationDebut'] < $date_ajd > $data['participationFin'])
-            // {
-                
-            // }
-            // else
-            // {
-            //     echo "pas les bonne dates";
-            // }
+            // Verication de la date de participation si comprise dans les temps de participation
+            
         }
         else
         {
--- Sprint/1/creation bdd.sql
+++ Sprint/1/creation bdd.sql
@@ -3,42 +3,43 @@ CREATE DATABASE bdd_gedimagination DEFAULT CHARSET=utf8;
 USE bdd_gedimagination;
 
 CREATE TABLE Utilisateur(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    email varchar(150) UNIQUE,
-    mdp varchar(120),
+    id_user INT PRIMARY KEY AUTO_INCREMENT,
     nom varchar(40),
     prenom varchar(40),
+    email varchar(150) UNIQUE,
+    mdp varchar(120),
     statut varchar(1)
 );
 
 CREATE TABLE Realisation(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    photo varchar(500),
+    id_real INT PRIMARY KEY AUTO_INCREMENT,
+    photo_url varchar(300),
+    photo_name varchar(50),
     titre varchar(100),
     descriptif varchar(150),
-    dateDebut DATE,
-    dateFin DATE,
-    dateParticipation DATE,
+    date_debut DATE,
+    date_fin DATE,
+    date_participation DATE,
     id_utilisateur INT,
-    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur(id)
+    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur(id_user)
 );
 
 CREATE TABLE Concours(
-    participationDebut DATE,
-    participationFin DATE
+    participation_debut DATE,
+    participation_fin DATE
 );
 
 /*  Requète d'insert */ 
-INSERT INTO Utilisateur (email, mdp, nom, prenom, statut) VALUES ('tanguy@gmail.com', 'tanguy1234', 'AVET', 'Tanguy', 'G');
-INSERT INTO Utilisateur (email, mdp, nom, prenom, statut) VALUES ('efeliott@gmail.com', 'eliott1234', 'FERTILLE', 'Eliott', 'P');
-INSERT INTO Utilisateur (email, mdp, nom, prenom, statut) VALUES ('yfyannis@gmail.com', 'yannis1234', 'FOUREL', 'Yannis', 'P');
-INSERT INTO Utilisateur (email, mdp, nom, prenom, statut) VALUES ('alayrton@gmail.com', 'ayrton1234', 'LORIAU', 'Ayrton', 'P');
-INSERT INTO Utilisateur (email, mdp, nom, prenom, statut) VALUES ('leoberger@gmail.com', 'leo1234', 'BERGER', 'Leo', 'P');
+INSERT INTO Utilisateur (nom, prenom, email, mdp, statut) VALUES ('AVET', 'Tanguy', 'tanguy@gmail.com', 'tanguy1234', 'G');
+INSERT INTO Utilisateur (nom, prenom, email, mdp, statut) VALUES ('FERTILLE', 'Eliott', 'efeliott@gmail.com', 'eliott1234', 'P');
+INSERT INTO Utilisateur (nom, prenom, email, mdp, statut) VALUES ('FOUREL', 'Yannis', 'yfyannis@gmail.com', 'yannis1234', 'P');
+INSERT INTO Utilisateur (nom, prenom, email, mdp, statut) VALUES ('LORIAU', 'Ayrton', 'alayrton@gmail.com', 'ayrton1234', 'P');
+INSERT INTO Utilisateur (nom, prenom, email, mdp, statut) VALUES ('BERGER', 'Leo', 'leoberger@gmail.com', 'leo1234', 'P');
 
-INSERT INTO Realisation (photo, titre, descriptif, dateDebut, dateFin, dateParticipation) VALUES ('url photo', 'Salon de jardin', 'Beau jardin ...', '2019-05-14', '2019-06-02', '2022-12-15');
-INSERT INTO Realisation (photo, titre, descriptif, dateDebut, dateFin, dateParticipation) VALUES ('url photo', 'Cuisine', 'Cuisine moderne ...', '2021-02-25', '2021-04-10', '2022-12-13');
-INSERT INTO Realisation (photo, titre, descriptif, dateDebut, dateFin, dateParticipation) VALUES ('url photo', 'Salle de bain', 'Salle de bain avec pierre ...', '2022-01-10', '2022-01-22', '2022-12-21');
-INSERT INTO Realisation (photo, titre, descriptif, dateDebut, dateFin, dateParticipation) VALUES ('url photo', 'Terrasse', 'Terrasse avec fausse pelouse ...', '2018-07-15', '2018-08-26', '2022-12-11');
-INSERT INTO Realisation (photo, titre, descriptif, dateDebut, dateFin, dateParticipation) VALUES ('url photo', 'Salon', 'Salon avec parquet claire ...', '2021-01-14', '2021-02-02', '2022-12-09');
+INSERT INTO Realisation (photo_url, photo_name, titre, descriptif, date_debut, date_fin, date_participation) VALUES ('url photo', 'nom photo', 'Salon de jardin', 'Beau jardin ...', '2019-05-14', '2019-06-02', '2022-12-15');
+INSERT INTO Realisation (photo_url, photo_name, titre, descriptif, date_debut, date_fin, date_participation) VALUES ('url photo', 'nom photo', 'Cuisine', 'Cuisine moderne ...', '2021-02-25', '2021-04-10', '2022-12-13');
+INSERT INTO Realisation (photo_url, photo_name, titre, descriptif, date_debut, date_fin, date_participation) VALUES ('url photo', 'nom photo', 'Salle de bain', 'Salle de bain avec pierre ...', '2022-01-10', '2022-01-22', '2022-12-21');
+INSERT INTO Realisation (photo_url, photo_name, titre, descriptif, date_debut, date_fin, date_participation) VALUES ('url photo', 'nom photo', 'Terrasse', 'Terrasse avec fausse pelouse ...', '2018-07-15', '2018-08-26', '2022-12-11');
+INSERT INTO Realisation (photo_url, photo_name, titre, descriptif, date_debut, date_fin, date_participation) VALUES ('url photo', 'nom photo', 'Salon', 'Salon avec parquet claire ...', '2021-01-14', '2021-02-02', '2022-12-09');
 
-INSERT INTO Concours (participationDebut, participationFin) VALUES ('2022-12-01', '2022-12-31');
+INSERT INTO Concours (participation_debut, participation_fin) VALUES ('2022-12-01', '2022-12-31');
