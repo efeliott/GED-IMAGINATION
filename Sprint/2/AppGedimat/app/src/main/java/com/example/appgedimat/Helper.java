88c1a8695acd2d6edf0cef2877486701dfbac73f
Eliott <efeliott@gmail.com>
2022-12-16 11:48:29 +0100

    update 16/12 clerc

--- Sprint/2/AppGedimat/app/src/main/java/com/example/appgedimat/Helper.java
+++ Sprint/2/AppGedimat/app/src/main/java/com/example/appgedimat/Helper.java
@@ -8,16 +8,17 @@ import androidx.annotation.Nullable;
 
 public class Helper extends SQLiteOpenHelper {
     public Helper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+        super(context, "GedimaginationBdd", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("CREATE TABLE Realisation(id_real INTEGER PRIMARY KEY, photo_url VARCHAR, photo_name VARCHAR, titre VARCHAR, description VARCHAR, debu_travaux DATE, fin_travaux DATE)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS Realisation");
+        onCreate(db);
     }
 }
